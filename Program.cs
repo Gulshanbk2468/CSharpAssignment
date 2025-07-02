265e230e0a84bcc4d204e269e7d9466b8508cc84
Gulshanbk2468 <gulshanbk11@gmail.com>
2025-07-01 23:20:02 -0700

    Arithmetic commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Program{
+
+    public static void Main(string[] args)
+    {
+        Arithmetic arithmetic = new Arithmetic();
+        arithmetic.Add(10,20);
+        arithmetic.Substract(20,10);
+        arithmetic.Multiply(10,20);
+        arithmetic.Divide(20,5);
+}
+}
+
+
