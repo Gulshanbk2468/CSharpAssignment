306a508aca9f0f31f5f4f3a1086824e693af71f9
Gulshanbk2468 <gulshanbk11@gmail.com>
2025-08-10 08:21:29 +0545

    uUpdated Compare.cs

--- Compare.cs
+++ Compare.cs
@@ -1,26 +1,43 @@
 using System;
+
 namespace ConsoleApp1
 {
     public class Compare
     {
         public void Cmp()
         {
-            Console.WriteLine("Enter first number:");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second number:");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            while (true) // infinite loop
+            {
+                Console.WriteLine("\nType 'end' at any time to quit.");
 
-            if (num1 > num2)
-            {
-                Console.WriteLine("The first number is greater than the second number.");
-            }else if (num1 < num2)
-            {
-                Console.WriteLine("The first number is less than the second number.");
+                Console.Write("Enter first number: ");
+                string input1 = Console.ReadLine();
+                if (input1.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                Console.Write("Enter second number: ");
+                string input2 = Console.ReadLine();
+                if (input2.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                int num1 = Convert.ToInt32(input1);
+                int num2 = Convert.ToInt32(input2);
+
+                if (num1 > num2)
+                {
+                    Console.WriteLine("The first number is greater than the second number.");
+                }
+                else if (num1 < num2)
+                {
+                    Console.WriteLine("The first number is less than the second number.");
+                }
+                else
+                {
+                    Console.WriteLine("Both numbers are equal.");
+                }
             }
-            else
-            {
-                Console.WriteLine("Both numbers are equal.");
-            }
+
+            Console.WriteLine("Program exited.");
         }
     }
 }
