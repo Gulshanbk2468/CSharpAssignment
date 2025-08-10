072817250d81c244dc333a08addaae4ad4711087
Gulshanbk2468 <gulshanbk11@gmail.com>
2025-08-10 08:41:02 +0545

    Updated version

--- GradeChecker.cs
+++ GradeChecker.cs
@@ -4,29 +4,44 @@ namespace ConsoleApp1
     class GradeChecker
     {
         public void Setdata()
-        {   Console.WriteLine("Welcome to the Grade Checker!");
-            Console.Write("Enter first grade: ");
-            double grade1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter second grade: ");
-            double grade2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter third grade: ");
-            double grade3 = Convert.ToDouble(Console.ReadLine());
-
-            double average = (grade1 + grade2 + grade3) / 3;
-
-            string result = average switch
+        {
+            while (true)
             {
-                >= 90 and <= 100 => "A+",
-                >= 80 and < 90 => "A",
-                >= 70 and < 80 => "B",
-                >= 60 and < 70 => "C",
-                >= 50 and < 60 => "D",
-                >= 0 and < 50 => "F",
-                _ => "Invalid marks"
-            };
+                Console.WriteLine("\nType 'exit' at any time to quit.");
+                Console.WriteLine("Welcome to the Grade Checker!");
+                Console.Write("Enter first grade: ");
+                string grade1 = Console.ReadLine();
+                if (grade1.Equals("exit" , StringComparison.OrdinalIgnoreCase))
+                    break;
+                Console.Write("Enter second grade: ");
+                string grade2 = Console.ReadLine();
+                if (grade2.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                    break;
+                Console.Write("Enter third grade: ");
+                string grade3 = Console.ReadLine();
+                if (grade2.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                    break;      
 
-            Console.WriteLine($"Your grade is: {result}");
+                double num1 = Convert.ToDouble(grade1);
+                double num2 = Convert.ToDouble(grade2);
+                double num3 = Convert.ToDouble(grade3);
+
+                double average = (num1 + num2 + num3) / 3;
+
+                string result = average switch
+                {
+                    >= 90 and <= 100 => "A+",
+                    >= 80 and < 90 => "A",
+                    >= 70 and < 80 => "B",
+                    >= 60 and < 70 => "C",
+                    >= 50 and < 60 => "D",
+                    >= 0 and < 50 => "F",
+                    _ => "Invalid marks"
+                };
+
+                Console.WriteLine($"Your grade is: {result}");
+            }
         }
-   };
+    }
 }
     
